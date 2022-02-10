fb5dd853f31cc7db461b5aafe002dc7f59625472
Prakash Agrahari <sshivamprakashh301@gmail.com>
2022-02-10 12:32:35 +0530

    feat: avatars and badges are integrated

---
+++ index.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", (inItEvent) => {
+  document.getElementsByClassName("head-logo")[0].style.transform =
+    "scale(1,1)";
+});
+
+window.addEventListener("load", (inItEvent) => {
+  document.getElementsByClassName("head")[0].style.transform =
+    "translateX(0%)";
+});
