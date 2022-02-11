235a2d6f78b47ac7045319ef097e749080067133
Prakash Agrahari <sshivamprakashh301@gmail.com>
2022-02-12 01:39:04 +0530

    Inputs added

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ window.addEventListener("load", (inItEvent) => {
 });
 
 window.addEventListener("load", (inItEvent) => {
-  document.getElementsByClassName("head")[0].style.transform =
-    "translateX(0%)";
+  document.getElementsByClassName("head")[0].style.transform = "translateX(0%)";
+});
+
+const inputText = document.getElementById("inputText");
+const errorOrSuccessInput = document.getElementById("errorOrSuccessInput");
+
+inputText.addEventListener("input", (e) => {
+  if (e.target.value.length + 1 > 4) {
+    errorOrSuccessInput.innerHTML = "Here you go!";
+    errorOrSuccessInput.style.color = "green";
+  } else {
+    errorOrSuccessInput.innerHTML = "input atleast 4 numbers";
+    errorOrSuccessInput.style.color = "red";
+  }
 });
