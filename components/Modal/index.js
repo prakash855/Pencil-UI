5c8b3870e0e106c6b0d46237e1e6f28115b90822
Prakash Agrahari <sshivamprakashh301@gmail.com>
2022-02-15 11:43:25 +0530

    refactor: es6 syntax added for JS

--- components/Modal/index.js
+++ components/Modal/index.js
@@ -5,19 +5,15 @@ var span = document.getElementsByClassName("close")[0];
 
 var modal = document.getElementById("myModal");
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// When the user clicks the button, open the modal
+btn.onclick = () => (modal.style.display = "block");
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+span.onclick = () => (modal.style.display = "none");
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+
+window.addEventListener(
+  "click",
+  (event) => event.target == modal && (modal.style.display = "none")
+);
